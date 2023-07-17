b2ae44489d5c70b04ba6605a698e846e8dffd0b0
hassan010802002 <92964808+hassan010802002@users.noreply.github.com>
2023-07-17 22:29:37 +0300

    Final Update

--- Assignments/Dart_Assignments/dart_assignment_2/bin/dart_assignment_2.dart
+++ Assignments/Dart_Assignments/dart_assignment_2/bin/dart_assignment_2.dart
@@ -1,6 +1,7 @@
 //this is a Dart Assignment 2......
 // ignore_for_file: non_constant_identifier_names
 import 'dart:io';
+import 'dart:js_interop';
 
 /// it contains 2 parts(capitalizing every first letter in each word in an entered sentence , search method in a list and find a record)
 void main(List<String> arguments) {
@@ -18,7 +19,69 @@ void main(List<String> arguments) {
 /**_______________________________________________________________________________________________ */
 
   /**the second part in the assignment .......!!!!!!!!!!! */
-  
+
+  var idKey = "id",
+      nameKey = "name",
+      ageKey = "age",
+      emailKey = "email",
+      phoneKey = "phone",
+      addressKey = "address";
+  String? nameValue, ageValue, emailValue, phoneValue, addressValue;
+  int idValue = 0;
+  List<Map<String, dynamic>>? data = [];
+  RegExp exp = RegExp(r'\w*');
+  Map<String, dynamic>? tempData;
+  while (true) {
+    print("!!!!!!!.......Enter Your Name : ......!!!!!!!!!!");
+    nameValue = stdin.readLineSync();
+    tempData!.addAll({
+      idKey: idValue++,
+      nameKey:
+          nameValue!.isNotEmpty && exp.hasMatch(nameValue) ? nameValue : null
+    });
+    print("!!!!!!!.......Enter Your Age : ......!!!!!!!!!!");
+    ageValue = stdin.readLineSync();
+    exp = RegExp(r'\d{2}');
+    tempData.addAll({
+      ageKey: ageValue!.isNotEmpty && exp.hasMatch(ageValue) ? ageValue : null
+    });
+    print("!!!!!!!.......Enter Your email : ......!!!!!!!!!!");
+    emailValue = stdin.readLineSync();
+    exp = RegExp(r'\w*\d*@gmail.com');
+    tempData.addAll({
+      emailKey:
+          emailValue!.isNotEmpty && exp.hasMatch(emailValue) ? emailValue : null
+    });
+    print("!!!!!!!.......Enter Your phone : ......!!!!!!!!!!");
+    phoneValue = stdin.readLineSync();
+    exp = RegExp(r'201[1|2|5]\d{8}');
+    tempData.addAll({
+      phoneKey:
+          phoneValue!.isNotEmpty && exp.hasMatch(phoneValue) ? phoneValue : null
+    });
+    print("!!!!!!!.......Enter Your address : ......!!!!!!!!!!");
+    addressValue = stdin.readLineSync();
+    exp = RegExp(r'\w*');
+    tempData.addAll({
+      addressKey: addressValue!.isNotEmpty && exp.hasMatch(addressValue)
+          ? addressValue
+          : null
+    });
+    if (nameValue.isNull &&
+        ageValue.isNull &&
+        emailValue.isNull &&
+        phoneValue.isNull &&
+        addressValue.isNull) {
+      tempData.clear();
+      break;
+    } else {
+      data.add(tempData);
+      tempData.clear();
+    }
+  }
+  print("Enter Your Name:.....");
+  String? searchName = stdin.readLineSync();
+  search(data, searchName);
 }
 
 String? getName(String? WholeSentence) {
@@ -41,8 +104,9 @@ String? getName(String? WholeSentence) {
       index++;
     }
   }
-///this is a second way ..........!!!!!!!!!!!!!
-///Just remove the comments from the following code and comment the previous ......!!!!!!!
+
+  ///this is a second way ..........!!!!!!!!!!!!!
+  ///Just remove the comments from the following code and comment the previous ......!!!!!!!
   // words.forEach((element) {
   //   newSentence = element.substring(0, 1).toUpperCase() + element.substring(1).toLowerCase();
   //   words[index] = "$newSentence ";
@@ -50,4 +114,24 @@ String? getName(String? WholeSentence) {
   // });
   WholeSentence = WholeSentence.isNotEmpty ? words.join().toString() : null;
   return WholeSentence;
+}
+
+void search(List<Map<String, dynamic>>? myData, String? name,
+    {String? id, String? email, String? phone}) {
+  print("!!!!!!!!!!!_______________Your Data________________!!!!!!!!!!!!!");
+  for (var element in myData!) {
+    if (element.containsValue(name) ||
+        element.containsValue(email) ||
+        element.containsValue(phone) ||
+        element.containsValue(id)) {
+      print("The ID is : ${element['id']}");
+      print("The Name is : ${element['name']}");
+      print("The Email is : ${element['email']}");
+      print("The Age is : ${element['age']}");
+      print("The Phone is : ${element['phone']}");
+      print("The Address is : ${element['address']}");
+    } else {
+      print("There No Such a Record in the List Containing This Name");
+    }
+  }
 }
