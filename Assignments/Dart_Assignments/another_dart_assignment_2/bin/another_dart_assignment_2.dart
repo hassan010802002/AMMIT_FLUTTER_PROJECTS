1dfdf73501c24bc628d873eeeafd4ecd1b9545d5
hassan010802002 <92964808+hassan010802002@users.noreply.github.com>
2023-07-23 11:23:44 +0300

    new assignment

---
+++ Assignments/Dart_Assignments/another_dart_assignment_2/bin/another_dart_assignment_2.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+//this is another dart assignment NO:2 _______________!!!!!!!!!!!!!
+///
+///in this Program I allow the User to Enter some comma separated numbers and Print these Numbers
+///separated and indexed while including if the user while entering the numbers if these numbers
+///contains any letters or other Strings like(spaces or commas or any thing else rather than numbers)
+///it will be stored in the list as a null value and will not be printed
+///
+
+void main(List<String> args) {
+  print(
+      "!!!!!!!!___________Enter Some Comma Separated Numbers ___________!!!!!!!!!");
+  String? number = stdin.readLineSync();
+  List<String>? myNumbers = [];
+  List<num?>? theNumbers = [];
+  if (number!.isEmpty) {
+    print("Nothing Entered Please Try Again");
+  } else {
+    if (number.endsWith(" ") || number.endsWith(",")) {
+      number = number.substring(0, number.length - 1);
+    }
+    myNumbers = number.split(",");
+    for (var element in myNumbers) {
+      if (element.endsWith(" ")) {
+        element = element.substring(0, element.length - 1);
+      }
+      theNumbers.add(num.tryParse(element));
+    }
+    print("My Numbers List _______ : $theNumbers");
+    int i = 1;
+    theNumbers.removeWhere((element) => element == null);
+    for (var element in theNumbers) {
+      print("${i++} - $element");
+    }
+  }
+}
+///!!!!!!!!!!!!___________________END____________________!!!!!!!!!!!!!!!
+///created by : Hassan Saied Hassan
---
+++ Assignments/Dart_Assignments/another_dart_assignment_2/test/another_dart_assignment_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:another_dart_assignment_2/another_dart_assignment_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
