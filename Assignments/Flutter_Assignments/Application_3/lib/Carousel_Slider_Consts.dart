04ecc4b525183ee522d499ea288443942a27f663
hassan010802002 <hassan01001820578@gmail.com>
2023-08-29 06:09:31 +0300

    assignment_3

---
+++ Assignments/Flutter_Assignments/Application_3/lib/Carousel_Slider_Consts.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CarouselSliderConsts{
+  static const titleText = "Home";
+  static const titleTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 38.0,
+    fontWeight: FontWeight.w900,
+  );
+  static const appBarColor = Color.fromRGBO(255, 0, 203, 1);
+  static const backgroundImage = AssetImage(
+    "assets/images/logo.png",
+  );
+  static const networkImagesList = [
+    "https://static01.nyt.com/images/2023/07/28/business/28strike-movies/28strike-movies-superJumbo.jpg",
+    "https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/07/movies-like-harry-potter.jpg",
+    "https://media.bulios.com/media/statuses/inside/c4de5c53-0804-4b81-90b0-2b2264311e97.jpg",
+    "https://mediafiles.cinema.com.hk/broadway/cmsimg/cinweb/movie/movie_1680166576.jpg",
+    "https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_465,q_auto,w_310/v1689361258/amc-cdn/production/2/movies/69100/69112/PosterDynamic/154778.jpg",
+    "https://cdn.marvel.com/content/1x/antmanandthewaspquantumania_lob_crd_03.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg",
+    "https://hips.hearstapps.com/hmg-prod/images/best-new-comedy-movies-2023-shazam-1670021757.jpg",
+    "https://thesmartlocal.my/wp-content/uploads/2023/01/new-movies-in-2023-scream-6.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Haunted_Mansion_%282023%29_poster.jpg/220px-Haunted_Mansion_%282023%29_poster.jpg"
+  ];
+
+  static const textStyle = TextStyle(
+      fontSize: 24.0,
+      color: Colors.white,
+      fontWeight: FontWeight.w600
+  );
+}
