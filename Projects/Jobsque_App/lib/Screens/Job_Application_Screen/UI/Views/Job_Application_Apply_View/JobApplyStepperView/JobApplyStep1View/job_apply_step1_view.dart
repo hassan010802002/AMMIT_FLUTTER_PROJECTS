5b01006da3534add656f275653b9b40adcef84d8
hassan010802002 <hassan01001820578@gmail.com>
2024-03-23 17:57:53 +0200

    JOBSQUE APP

--- Projects/Jobsque_App/lib/Screens/Job_Application_Screen/UI/Views/Job_Application_Apply_View/JobApplyStepperView/JobApplyStep1View/job_apply_step1_view.dart
+++ Projects/Jobsque_App/lib/Screens/Job_Application_Screen/UI/Views/Job_Application_Apply_View/JobApplyStepperView/JobApplyStep1View/job_apply_step1_view.dart
@@ -11,6 +11,7 @@ import 'package:jobsque_app/Screens/Job_Application_Screen/UI/Widgets/JobApplyPh
 import 'package:jobsque_app/Screens/Job_Application_Screen/UI/Widgets/JobApplyStep1EmailTextFieldWidget/job_apply_step1_email_text_field_widget.dart';
 import 'package:jobsque_app/Screens/Job_Application_Screen/UI/Widgets/JobApplyStep1NameTextFieldWidget/job_apply_step1_name_text_field_widget.dart';
 import 'package:jobsque_app/Screens/Job_Application_Screen/UI/Widgets/JobApplyStepTextWidget/job_apply_step_text_widget.dart';
+
 import '../../../../../Controllers/Application_Controller/application_cubit.dart';
 
 class JobApplyStep1View extends StatefulWidget {
@@ -26,40 +27,37 @@ class _JobApplyStep1ViewState extends State<JobApplyStep1View> {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.centerLeft,
-      child: SingleChildScrollView(
-        primary: true,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const JobApplyBioDataTextWidget(),
-            const JobApplyStepTextWidget(),
-            SizedBox(
-              height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 35.0.h,
-            ),
-            const JobApplyFullNameTextWidget(),
-            SizedBox(
-              height: 8.0.h,
-            ),
-            JobApplyStep1NameTextFieldWidget(applicationCubitController: widget.applicationCubitController),
-            SizedBox(
-              height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 35.0.h,
-            ),
-            const JobApplyEmailTextWidget(),
-            SizedBox(
-              height: 8.0.h,
-            ),
-            JobApplyStep1EmailTextFieldWidget(applicationCubitController: widget.applicationCubitController),
-            SizedBox(
-              height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 35.0.h,
-            ),
-            const JobApplyPhoneTextWidget(),
-            SizedBox(
-              height: 8.0.h,
-            ),
-            JobApplyPhoneField(applicationCubitController: widget.applicationCubitController),
-          ],
-        ),
+      alignment: Alignment.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const JobApplyBioDataTextWidget(),
+          const JobApplyStepTextWidget(),
+          SizedBox(
+            height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 35.0.h,
+          ),
+          const JobApplyFullNameTextWidget(),
+          SizedBox(
+            height: 8.0.h,
+          ),
+          JobApplyStep1NameTextFieldWidget(applicationCubitController: widget.applicationCubitController),
+          SizedBox(
+            height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 35.0.h,
+          ),
+          const JobApplyEmailTextWidget(),
+          SizedBox(
+            height: 8.0.h,
+          ),
+          JobApplyStep1EmailTextFieldWidget(applicationCubitController: widget.applicationCubitController),
+          SizedBox(
+            height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 35.0.h,
+          ),
+          const JobApplyPhoneTextWidget(),
+          SizedBox(
+            height: 8.0.h,
+          ),
+          JobApplyPhoneField(applicationCubitController: widget.applicationCubitController),
+        ],
       ),
     );
   }
--- Projects/Jobsque_App/lib/Screens/Job_Application_Screen/UI/Views/job_application_main_view.dart
+++ Projects/Jobsque_App/lib/Screens/Job_Application_Screen/UI/Views/job_application_main_view.dart
@@ -3,8 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jobsque_app/Helpers/Size_Helper/MediaQuery_Size_Helper.dart';
 import 'package:jobsque_app/Screens/Job_Application_Screen/UI/Views/Job_Application_Apply_Complete_View/job_application_apply_complete_view.dart';
 import 'package:jobsque_app/Screens/Job_Application_Screen/UI/Views/Job_Application_Apply_View/job_application_apply_view.dart';
+
 import '../../Controllers/Application_Controller/application_cubit.dart';
 import '../../Controllers/Details_Controller/details_cubit.dart';
 import '../../Controllers/job_application_cubit.dart';
@@ -33,24 +35,29 @@ class _JobApplicationMainViewState extends State<JobApplicationMainView> {
     return BlocBuilder<JobApplicationCubit, JobApplicationState>(
       bloc: widget.mainCubitController,
       builder: (context, state) {
-        return Align(
-          alignment: Alignment.center,
-          child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0.w, vertical: 15.0.h),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  (state is JobApplicationDetailsViewState || state is JobApplicationInitial) && widget.mainCubitController.isJobDetailsView
-                      ? JobApplicationDetailsView(detailsCubitController: widget.detailsCubitController)
-                      : state is JobApplicationApplyViewState && widget.mainCubitController.isJobApplyView
-                          ? JobApplicationApplyView(applicationCubitController: widget.applicationCubitController)
-                          : const JobApplicationApplyCompleteView(),
-                  JobApplicationButtonWidget(
-                    mainCubitController: widget.mainCubitController,
-                    applicationCubitController: widget.applicationCubitController,
-                  ),
-                ],
-              )),
+        return SingleChildScrollView(
+          primary: true,
+          child: Align(
+            alignment: Alignment.center,
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0.w, vertical: 15.0.h),
+                child: Column(
+                  children: [
+                    (state is JobApplicationDetailsViewState || state is JobApplicationInitial) && widget.mainCubitController.isJobDetailsView
+                        ? JobApplicationDetailsView(detailsCubitController: widget.detailsCubitController)
+                        : state is JobApplicationApplyViewState && widget.mainCubitController.isJobApplyView
+                            ? JobApplicationApplyView(applicationCubitController: widget.applicationCubitController)
+                            : const JobApplicationApplyCompleteView(),
+                    SizedBox(
+                      height: MediaQuery_Size_Helper.MAX_HEIGHT(context)! / 9.0.h,
+                    ),
+                    JobApplicationButtonWidget(
+                      mainCubitController: widget.mainCubitController,
+                      applicationCubitController: widget.applicationCubitController,
+                    ),
+                  ],
+                )),
+          ),
         );
       },
     );
